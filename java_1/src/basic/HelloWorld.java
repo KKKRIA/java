2de56a761e60b4f12b58210f0d4b675976dffa42
itwill <itwill@DESKTOP-7688MVJ>
2023-03-29 12:32:03 +0900

    lDay

---
+++ java_1/src/basic/HelloWorld.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.print("Java");
+		System.out.println("_Progamming");
+		System.out.println("Good Luck!!!");
+	}
+}
+
+
+ 
