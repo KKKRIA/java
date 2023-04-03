aed19b9f807fe44ad01b35bc5d75281939a2e4a2
itwill <itwill@DESKTOP-7688MVJ>
2023-04-03 18:27:14 +0900

    5Day

--- java_1/src/example/MultiforExample.java
+++ java_1/src/example/MultiforExample.java
@@ -8,6 +8,16 @@ public class MultiforExample {
 		//★★
 		//★
 
+		for(int i=5; i>=1; i--) {
+			for(int j=1; j<=i; j++ ) {
+				System.out.print("★");
+			}
+			System.out.println("");
+		}
+		
+		
+		
+		
 
 		System.out.println("===============================================================");
 		//★★★★★
@@ -16,7 +26,35 @@ public class MultiforExample {
 		//☆☆☆★★
 		//☆☆☆☆★
 
+		
+		
+		for(int i=5; i>=1; i--) {
+			for(int j=1; j<=i; j++ ) {
+				System.out.print("★");
+			}
+			for(int j=1; j<=(5-i); j++ ) {
+				System.out.print("☆");
+			}
+			System.out.println("");
+		}
+		
+		
+		
+		/*
+		for(int i=1; i<=5; i++) {//행 
+			 for(int j=1; j<=(5-i); j++) {//열 
+				 System.out.print("☆");
+			 }
+			 for(int j=1; j<=i; j++) {//열 
+				 System.out.print("★");
+			 }
+			 System.out.println();
+		 }
+		*/
+	
 
+		 
+		
 		System.out.println("===============================================================");
 		//★
 		//★★★
@@ -24,6 +62,17 @@ public class MultiforExample {
 		//★★★★★★★
 		//★★★★★★★★★
 
+		
+		for(int i=1; i<=5; i++) {
+			/*for(int j=1; j<=(int)(i%3==0); j++) */{
+				System.out.print("★");
+			}
+			System.out.println();
+		}
+		
+		
+	
+		
 
 		System.out.println("===============================================================");
 		//★☆☆☆★
