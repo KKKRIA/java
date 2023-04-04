d7d6b2198f7a40e0a4c256f4836c6a4562892a0c
itwill <itwill@DESKTOP-7688MVJ>
2023-04-04 10:07:42 +0900

    5Day

--- java_1/src/example/MultiforExample.java
+++ java_1/src/example/MultiforExample.java
@@ -1,87 +1,126 @@
 package example;
 
+
+
 public class MultiforExample {
+
 	public static void main(String[] args) {
+
 		//★★★★★
 		//★★★★
 		//★★★
 		//★★
 		//★
 
+
+
 		for(int i=5; i>=1; i--) {
 			for(int j=1; j<=i; j++ ) {
 				System.out.print("★");
 			}
 			System.out.println("");
 		}
-		
-		
-		
+
 		
 
+		
+
+		
+
+		
+
+
+
 		System.out.println("===============================================================");
+
 		//★★★★★
 		//☆★★★★
 		//☆☆★★★
 		//☆☆☆★★
 		//☆☆☆☆★
 
-		
-		
-		for(int i=5; i>=1; i--) {
-			for(int j=1; j<=i; j++ ) {
-				System.out.print("★");
-			}
-			for(int j=1; j<=(5-i); j++ ) {
-				System.out.print("☆");
+
+		for(int i=1; i<=5; i++) {
+
+			for(int j=1; j<=5; j++) {
+				if(j<i) {
+					 System.out.print("☆");
+				}
+				else {
+					 System.out.print("★");
+				 }
 			}
 			System.out.println("");
 		}
+
 		
+
 		
-		
-		/*
-		for(int i=1; i<=5; i++) {//행 
-			 for(int j=1; j<=(5-i); j++) {//열 
-				 System.out.print("☆");
-			 }
-			 for(int j=1; j<=i; j++) {//열 
-				 System.out.print("★");
-			 }
-			 System.out.println();
-		 }
-		*/
+
 	
 
+
+
 		 
+
 		
+
 		System.out.println("===============================================================");
+
 		//★
 		//★★★
 		//★★★★★
 		//★★★★★★★
 		//★★★★★★★★★
 
-		
-		for(int i=1; i<=5; i++) {
-			/*for(int j=1; j<=(int)(i%3==0); j++) */{
+	/*	
+		int count=1;
+
+		for(int i=1; i<=5; i++) { //행
+			for(int j=1; j<=count; j++) { //열
 				System.out.print("★");
 			}
 			System.out.println();
+			count +=2;
 		}
-		
-		
-	
+*/
 		
 
+
+		for(int i=1; i<=9; i+=2) { //행
+			for(int j=1; j<=9; j++) {//열
+				if(j<=i){
+				System.out.print("★");
+				}
+			}
+			System.out.println();
+		
+		}
+		
+
+	
+
+		
+
+
+
 		System.out.println("===============================================================");
+
 		//★☆☆☆★
 		//☆★☆★☆
 		//☆☆★☆☆
 		//☆★☆★☆
 		//★☆☆☆★
 
+
 		
+
+
+
+					
+
 		System.out.println("===============================================================");
+
 	}
+
 }
