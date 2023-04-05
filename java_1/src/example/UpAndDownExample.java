686f1720787fe16c77793e868de7908109920827
itwill <itwill@DESKTOP-7688MVJ>
2023-04-05 09:34:48 +0900

    6Day

--- java_1/src/example/UpAndDownExample.java
+++ java_1/src/example/UpAndDownExample.java
@@ -2,51 +2,101 @@ package example;
 
 import java.util.Scanner;
 
+
 //컴퓨터로부터 제공받은 정수 난수값을 키보드로 입력하여 맞추는 프로그램을 작성하세요.
-// => 1~100 범위의 정수 난수값을 제공받도록 작성
+// => 1~100 범위의 정수 난수값을 제공받도록 작성 0
+
 // => 난수값을 맞출 수 있는 기회는 10번만 제공되도록 작성
-// => 키보드 입력값이 1~100 범위가 아닌 경우 메세지 출력 후 재입력
+
+// => 키보드 입력값이 1~100 범위가 아닌 경우 메세지 출력 후 재입력 0
 
 // => 난수값과 입력값이 같은 경우 입력 횟수 출력 후 프로그램 종료
+
 // => 난수값과 입력값이 다른 경우 "큰값 입력" 또는 "작은값 입력" 형식의 메세지 출력
 // => 난수값을 10번 안에 맞추지 못한 경우 난수값이 출력되도록 작성
 
+
+
 public class UpAndDownExample {
+
 	public static void main(String[] args) {
 		Scanner scanner=new Scanner(System.in);
-		
-		int count=0;
-		
+		//int count=0;
+
 		// => 1~100 범위의 정수값을 난수로 제공받아 저장
-		int computer=(int)(Math.random()*100)+1;
-		
-		//사용자에게 난수값 하나를 키보드로 입력받아 저장 *
+
+		int su=(int)(Math.random()*100)+1;
+
+		//사용자에게 난수값 하나를 키보드로 입력받아 저장 
 		// => 입력값이 1~100 범위가 아닌 경우 메세지 출력 후 재입력
-		
+
 		int user;
-		int cnt=10;
+		int cnt; //10번
+
+		for(cnt=1;cnt<=10;cnt++) {
+
+			while(true) {
+				System.out.println("난수값을 입력하세요 >>");
+				user=scanner.nextInt();
+
+				if(user>=1 && user<=100) break;
+				System.out.println("[에러] 1~100의 범위가 아닙니다." );
+			}
 		
-		while(true) {
-			System.out.println("난수값을 입력하세요 >>");
-			user=scanner.nextInt();
-			if(user>=1 && user<=100) break;
-			System.out.println("[에러] 1~100의 범위가 아닙니다." );
+
+			if(su==user) {
+				System.out.println("정답입니다");break;
+			}
+			if(su>user) {
+				System.out.println("큰값 입력");
+			}else {
+				System.out.println("작은값 입력");
+			}
 		}
+
+	
+		if(cnt == 11) {
+				System.out.println("10번안에 못맞춤, 답 = "+su);
+			}
+
 		
+
 		
+
+		
+
+		
+
 		// => 난수값과 입력값이 같은 경우 입력 횟수 출력 후 프로그램 종료
-		//컴퓨터 출력
-		System.out.println("[입력]컴퓨터 >> ");
+
 		
-		//사용자 출력
-		System.out.println("[입력]사용자 >> ");
+
 		
+
+		/*for(int i=0; i<su; i++) {
+
+			if(su==user) break; {
+
+			System.out.println("입력횟수 = "+ cnt);
+
+			}
+
+		}*/
+
 		
+
 		
+
 		
+
 		
+
 		
-		
+
+
+
 	}
+
 }
 
+	
