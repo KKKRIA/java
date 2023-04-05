cc8861fe924864816bd6c0b89cfe72b80a83e90e
itwill <itwill@DESKTOP-7688MVJ>
2023-04-05 09:43:13 +0900

    6Day

--- java_1/src/basic/MathRandomApp.java
+++ java_1/src/basic/MathRandomApp.java
@@ -7,7 +7,7 @@ package basic;
 public class MathRandomApp {
 	public static void main(String[] args) {
 		//Math 클래스 : 수학 관련 기능을 제공하는 클래스
-		// Math.random() :0.0보다 크거나 같고 1.0보다 작은 실수 난수값을 반환하는 메소드
+		//Math.random() :0.0보다 크거나 같고 1.0보다 작은 실수 난수값을 반환하는 메소드
 		
 		//=>  0.0 <= 난수값 <1.0
 		for(int i=1; i<5; i++) {
