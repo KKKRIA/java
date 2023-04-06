7b2e5245ff47573ddc80bd4a8237a068da11f903
itwill <itwill@DESKTOP-7688MVJ>
2023-04-06 11:59:52 +0900

    7Day

--- java_2/src/oop/ArraySortApp.java
+++ java_2/src/oop/ArraySortApp.java
@@ -5,13 +5,18 @@ import java.util.Arrays;
 //OOP(Object Oriented Programming) : 객체 지향 프로그래밍
 // => 객체를 이용하여 프로그램을 작성하는 방식
 // => 현실에 존재하는 대상들을 모델링하여 클래스로 선언하고 선언된 클래스로 객체를 생성하여
+
 //객체의 요소를 사용하여 프로그램을 작성하는 방법
 // => 추상화, 캡슐화, 다형성, 상속성
+
 //객체 모델링(Object Modeling) : 현실에 존재하는 대상을 속성과 행위로 구분하여 설계
 // => 설계된 대상을 클래스(필드와 메소드)로 선언 - 추상화(Abstraction)
+
 //클래스 배포 가능 : 프로그램의 생산성 증가 및 유지보수의 효율성 증가
 
 //배열에 저장된 모든 요소값을 오름차순 정렬하여 출력하는 프로그램 작성
+
+
 public class ArraySortApp {
 	public static void main(String[] args) {
 		/*
--- java_2/src/oop/MethodApp.java
+++ java_2/src/oop/MethodApp.java
@@ -12,23 +12,33 @@ public class MethodApp {
 		//참조변수를 출력할 경우 "자료형@메모리주소" 형식의 문자열을 제공받아 출력
 		System.out.println("method = "+method);
 		System.out.println("==============================================================");
+		
+		
 		//객체를 이용하여 메소드를 호출하면 프로그램의 흐름(스레드)이 객체의 메소드로 이동하여
 		//메소드의 명령을 실행하고 메소드가 종료되면 메소드를 호출한 다음 차례의 명령 실행
 		method.displayOne();
 		method.displayTwo();
 		method.displayOne();
 		System.out.println("==============================================================");
+		
+		
 		method.printOne();
 		method.printOne();
 		System.out.println("==============================================================");
+		
+		
 		method.printTwo(50);
 		method.printTwo(80);
 		method.printTwo(-30);
 		System.out.println("==============================================================");
+		
+		
 		method.printThree(35, 79);
 		method.printThree(19, 88);
 		method.printThree(67, 12);
 		System.out.println("==============================================================");
+		
+		
 		//반환값이 존재하는 메소드 호출  
 		// => 메소드의 반환값을 지속적으로 사용하고자 할 경우 반환값을 변수에 저장
 		int result=method.returnTot(30, 70);
@@ -36,6 +46,8 @@ public class MethodApp {
 		//메소드의 반환값을 일시적으로 사용할 경우 반환값을 변수에 미저장
 		System.out.println("합계(메소드 호출의 반환값) = "+method.returnTot(20, 80));
 		System.out.println("==============================================================");
+		
+		
 		boolean result1=method.isOddEven(10);
 		if(result1) {
 			System.out.println("매개변수로 전달된 값은 [짝수]입니다.");
@@ -50,6 +62,8 @@ public class MethodApp {
 			System.out.println("매개변수로 전달된 값은 [홀수]입니다.");
 		}
 		System.out.println("==============================================================");
+		
+		
 		//배열의 메모리 주소를 반환하는 메소드를 호출하면 반환된 메모리 주소를 참조변수에 저장
 		// => 참조변수에 저장된 메모리 주소를 사용하여 배열 참조 가능
 		int[] array=method.returnArray();
@@ -58,12 +72,16 @@ public class MethodApp {
 		}
 		System.out.println();
 		System.out.println("==============================================================");
+		
+		
 		System.out.println("합계 = "+method.sumOne(10, 20, 30));
 		//매개변수에 값이 정상적으로 전달되지 않을 경우 메소드 호출에 대한 에러 발생
 		//System.out.println("합계 = "+method.sumOne());
 		//System.out.println("합계 = "+method.sumOne(10,20));
 		//System.out.println("합계 = "+method.sumOne(10,20,30,40));
 		System.out.println("==============================================================");
+		
+		
 		//System.out.println("합계 = "+method.sumTwo(10, 20, 30));//에러 발생
 		//매개변수의 배열의 메모리 주소를 전달하여 메소드 호출
 		System.out.println("합계 = "+method.sumTwo(new int[]{10, 20, 30}));
@@ -71,6 +89,8 @@ public class MethodApp {
 		System.out.println("합계 = "+method.sumTwo(new int[]{10, 20}));
 		System.out.println("합계 = "+method.sumTwo(new int[]{10, 20, 30, 40}));
 		System.out.println("==============================================================");
+		
+		
 		System.out.println("합계 = "+method.sumThree(10, 20, 30));
 		System.out.println("합계 = "+method.sumThree());
 		System.out.println("합계 = "+method.sumThree(10, 20));
