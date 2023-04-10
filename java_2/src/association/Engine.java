c920ac6f6d625d646170ef860fe737ae7c2d5779
itwill <itwill@DESKTOP-7688MVJ>
2023-04-10 14:45:55 +0900

    10Day

---
+++ java_2/src/association/Engine.java
@@ -0,0 +1,52 @@
+package association;
+
+
+//엔진정보(연료타입, 배기량)를 저장하기 위한 클래스
+public class Engine{
+	private String fualType;
+	private int displacement;
+	
+	
+	public Engine() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	//생성자
+	public Engine(String fualType, int displacement) {
+		super();
+		this.fualType = fualType;
+		this.displacement = displacement;
+	}
+
+	
+	
+	//setter getter
+	public String getFualType() {
+		return fualType;
+	}
+
+	public void setFualType(String fualType) {
+		this.fualType = fualType;
+	}
+
+	public int getDisplacement() {
+		return displacement;
+	}
+
+	public void setDisplacement(int displacement) {
+		this.displacement = displacement;
+	}
+	
+	
+	
+	//엔진정보(필드)를 출력하는 메소드
+	public void displayEngine() {
+		System.out.println("연료타입 = "+ fualType);
+		System.out.println("배기량 = "+ displacement);
+	}
+	
+	
+	
+}
