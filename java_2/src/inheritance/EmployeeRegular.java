bc1a7269056d708a82bfc8c9c951f683917e52b2
itwill <itwill@DESKTOP-7688MVJ>
2023-04-11 17:27:39 +0900

    11Day

--- java_2/src/inheritance/EmployeeRegular.java
+++ java_2/src/inheritance/EmployeeRegular.java
@@ -1,8 +1,7 @@
 package inheritance;
 
 //정규직 사원정보(사원번호, 사원이름, 연봉)를 저장하기 위한 클래스
-// => Employee클래스를 상속받아 작성
-
+// => Employee 클래스를 상속받아 작성
 public class EmployeeRegular extends Employee {
 	private int anuualSalary;
 	
@@ -10,8 +9,8 @@ public class EmployeeRegular extends Employee {
 		// TODO Auto-generated constructor stub
 	}
 
-	public EmployeeRegular(int empNO, String empName, int anuualSalary) {
-		super(empNO, empName);
+	public EmployeeRegular(int empNo, String empName, int anuualSalary) {
+		super(empNo, empName);
 		this.anuualSalary = anuualSalary;
 	}
 
@@ -23,13 +22,21 @@ public class EmployeeRegular extends Employee {
 		this.anuualSalary = anuualSalary;
 	}
 	
-	
-	//급여를 반환하는 메소드
-	public int computeSaSalary() {
+	//급여를 계산하여 반환하는 메소드
+	public int computeSalary() {
+		return anuualSalary/12;
+	}
+
+	@Override
+	public int computePay() {
 		return anuualSalary/12;
 	}
 	
-	
-	
-	
+	/*
+	//부모클래스의 final 메소드를 오버라이드 선언하여 에러 발생
+	@Override
+	public int computeIncentive() {
+		return 100000000;
+	}
+	*/
 }
