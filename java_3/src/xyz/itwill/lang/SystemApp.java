d99a239b691a43c306f13813dfb9e23b9ecd8564
itwill <itwill@DESKTOP-7688MVJ>
2023-04-14 18:00:13 +0900

    14Day

--- java_3/src/xyz/itwill/lang/SystemApp.java
+++ java_3/src/xyz/itwill/lang/SystemApp.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 //Java API (Application Programming Interface) : Java 프로그램 개발에 프로그램 기능을 제공하기 위한 도구
 // => 라이브러리를 이용하여 배포된 Java 자료형 (클래스, 인터페이스, 열거형)
 // => API문서를 활용하여 Java 자료형에 대한 설명을 제공받아 사용
-// => 온라인 API문서 : 
+// => 온라인 API문서 :  https://docs.oracle.com/en/java/javase/17/docs/api/index.html
 
 //Java.lang 패키지 : Java프로그램 개발에 필요한 기본 자료형을 제공하는 패키지
 // => java.lang 패키지의 자료형은 import 처리를 하지 않아도 접근 가능
@@ -18,10 +18,10 @@ import java.util.Scanner;
 //System 클래스 : 표준 입출력 스트림을 제공하거나 시스템 관련 기능을 메소드로 제공하는 클래스
 public class SystemApp {
 	public static void main(String[] args) {
-		//System.in : Java에서 제공되는 표준 입력장치(키보드)에 대한 입력스트림(객체)이 저장된 
+		//System.in : Java에서 제공되는 표준 입력장치(키보드)에 대한 입력스트림(객체)이 저장된 필드
 		Scanner scanner=new Scanner(System.in);
 		
-		//System.out : Java에서 제공되는 표준 출력장치(모니터)에 대한 출력스트림(객체)이 저장된 
+		//System.out : Java에서 제공되는 표준 출력장치(모니터)에 대한 출력스트림(객체)이 저장된 필드
 		System.out.println("정수값 입력 >> ");
 		
 		int num=scanner.nextInt();
@@ -39,7 +39,7 @@ public class SystemApp {
 		long startTime=System.currentTimeMillis();
 		
 		for(int i=1;i<num;i++) {
-			System.out.println(num+"번째 실행되는 명령");
+			System.out.println(i+"번째 실행되는 명령");
 		}
 		
 		long endTime=System.currentTimeMillis();
