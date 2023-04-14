07e237a6fba5a9a45e94de746499c2929e404a64
itwill <itwill@DESKTOP-7688MVJ>
2023-04-14 10:25:53 +0900

    13Day

--- java_2/src/xyz/itwill/app/SubjectApp.java
+++ java_2/src/xyz/itwill/app/SubjectApp.java
@@ -5,6 +5,7 @@ package xyz.itwill.app;
 // => Java 참조형 : 클래스(Class), 인터페이스(Interface), 열거형(Enum)
 // => Java 자료형을 보다 쉽게 관리하기 위해 패키지 사용
 // => 패키지의 이름은 도메인을 역방향으로 나열하고 그룹명을 지정하여 작성하는 것을 권장
+
 //ex) itwill.xyz : 도메인 >> xyz.itwill.board
 //도메인(Domain) : 인터넷에서 개인 또는 그룹이 사용하기 위한 네트워크 식별자
 
@@ -15,10 +16,13 @@ package xyz.itwill.app;
 //import 키워드 : 다른 패키지에 작성된 Java 자료형을 명확히 표현하여 접근하기 위한 키워드
 //형식) import 패키지경로.자료형;   >> 자료형 대신 *(전체) 사용 가능
 // => package 키워드로 작성된 명령 하단에 선언하며 자료형 위에 선언
+
 //이클립스에서는 다른 패키지에 작성된 자료형을 사용할 경우 import 명령을 자동 완성하는 기능 
 //제공 - [Ctrl]+[Space] => 같은 이름의 자료형이 여러개인 경우 선택
+
 // => [Ctrl]+[Shift]+[O] >> import 명령을 정리하는 단축키 : 불필요한 자료형에 대한 import
 //설정을 제거하거나 필요한 자료형에 대한 import 설정을 추가하는 기능 제공
+
 import xyz.itwill.subject.OracleSubject;
 import xyz.itwill.subject.JavaSubject;
 //자료형의 이름이 같은 경우 import 처리 불가능
@@ -28,8 +32,10 @@ public class SubjectApp {
 	public static void main(String[] args) {
 		//같은 패키지에 작성된 클래스는 패키지 경로 표현 없이 클래스 접근 가능
 		// => 형식) 클래스명
+		
 		//다른 패키지에 작성된 클래스는 반드시 패키지 경로를 표현해야만 클래스 접근 가능
 		// => 형식) 패키지경로.클래스명
+		
 		//xyz.itwill.subject.OracleSubject subject1=new xyz.itwill.subject.OracleSubject();
 		
 		//다른 패키지에 작성된 클래스를 import 처리한 경우 패키지 경로 표현없이 클래스 접근 가능
