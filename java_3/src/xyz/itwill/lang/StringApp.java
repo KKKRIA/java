e6a1a6bb65d643152bc1b2cc030017ee30163171
itwill <itwill@DESKTOP-7688MVJ>
2023-04-17 09:36:51 +0900

    14Day

--- java_3/src/xyz/itwill/lang/StringApp.java
+++ java_3/src/xyz/itwill/lang/StringApp.java
@@ -43,7 +43,7 @@ public class StringApp {
 		System.out.println("====================================================");
 		
 		
-		//String.equals(String str) : String 객체에 문자열과 매개변수로 전달받은 문자열을 비교하여 다른경우 [false]를 반환하고 같은 경우[true]를 반환하는 메소드
+		//★String.equals(String str) : String 객체에 문자열과 매개변수로 전달받은 문자열을 비교하여 다른경우 [false]를 반환하고 같은 경우[true]를 반환하는 메소드
 		// => 비교문자열이 영문자인 경우 대소문자를 구분하여 비교
 		if(str1.equals(str3)) {
 			System.out.println("str1변수와 str3 변수에 저장된 String 객체의 문자열이 같습니다.");
@@ -64,7 +64,7 @@ public class StringApp {
 		
 		
 		
-		//String.equalsIgnoreCase(String str) : String 객체에 문자열과 매개변수로 전달받은 문자열을 비교하여 다른경우 [false]를 반환하고 같은 경우[true]를 반환하는 메소드
+		//★String.equalsIgnoreCase(String str) : String 객체에 문자열과 매개변수로 전달받은 문자열을 비교하여 다른경우 [false]를 반환하고 같은 경우[true]를 반환하는 메소드
 		// => 비교문자열이 영문자인 경우 대소문자를 구분하지 않고 비교
 		if(str1.equalsIgnoreCase(str4)) {
 			System.out.println("str1변수와 str3 변수에 저장된 String 객체의 문자열이 같습니다.");
@@ -75,7 +75,7 @@ public class StringApp {
 		
 		
 		
-		//String.compareTo(String str) : String 객체에 저장된 문자열과 매개변수로 전달받은 문자열을 비교하여 
+		//★String.compareTo(String str) : String 객체에 저장된 문자열과 매개변수로 전달받은 문자열을 비교하여 
 		//String 객체의 문자열이 큰 경우 양수를 반환하고 매개변수로 전달받은 문자열이 큰 경우 음수를 반환하면 같은 경우 0을 반환하는 메소드
 		if(str1.compareTo(str4)>0) {
 			System.out.println("str1 변수에 저장된 문자열이 str4에 저장된 문자열보다 큽니다.");
@@ -88,7 +88,7 @@ public class StringApp {
 		
 		
 		
-		//String.getBytes() : String 객체에 저장된 문자열을 byte배열로 변환하여 반환하는 메소드
+		//★String.getBytes() : String 객체에 저장된 문자열을 byte배열로 변환하여 반환하는 메소드
 		// => byte 배열의 요소에는 문자열을 구성하는 문자들이 코드값(정수값)으로 차례대로 저장 
 		byte[] array=str1.getBytes();
 		
@@ -101,17 +101,17 @@ public class StringApp {
 		
 		
 		String str5="ABCDEFG";
-		//String.length() : String 객체에 저장된 문자열의 문자 갯수를 반환하는 메소드
+		//★String.length() : String 객체에 저장된 문자열의 문자 갯수를 반환하는 메소드
 		System.out.println("문자열의 문자 갯수 = "+str5.length());
 		System.out.println("====================================================");
 		
 		
-		//String.charAt(int index) : String 객체에 저장된 문자열에서 첨자(index) 위치의 문자를 반환하는 메소드
+		//★String.charAt(int index) : String 객체에 저장된 문자열에서 첨자(index) 위치의 문자를 반환하는 메소드
 		System.out.println("두번째 위치의 문자 =" +str5.charAt(1));
 		System.out.println("====================================================");
 		
 		
-		//String.indexOf(String index) : String 객체에 저장된 문자열에서 매개변수로 전달받은 문자열(문자)를 
+		//★String.indexOf(String index) : String 객체에 저장된 문자열에서 매개변수로 전달받은 문자열(문자)를 
 		//처음부터 차례대로 검색하여 시작위치값(첨자)를 반환하는 메소드
 		// => 매개변수로 전달받은 문자열을 찾을 수 없는 경우 -1반환
 		System.out.println("A문자열의 저장 위치 ="+str5.indexOf("A"));
@@ -123,9 +123,9 @@ public class StringApp {
 		String str6="Java Programming";
 		
 		System.out.println("str6 = "+str6);
-		//String.toUpperCase() : String 객체에 저장된 문자열을 모두 대문자로 변환하여 반환하는 메소드
+		//★String.toUpperCase() : String 객체에 저장된 문자열을 모두 대문자로 변환하여 반환하는 메소드
 		System.out.println("str6.toUpperCase() = "+str6.toUpperCase());
-		//String.toLowerCase() : String 객체에 저장된 문자열을 모두 소문자로 변환하여 반환하는 메소드
+		//★String.toLowerCase() : String 객체에 저장된 문자열을 모두 소문자로 변환하여 반환하는 메소드
 		System.out.println("str6.toLowerCase() = "+str6.toLowerCase());
 		System.out.println("==============================================================");
 		
@@ -133,7 +133,7 @@ public class StringApp {
 		String str7="     홍길동      ";
 		
 		System.out.println("입력된 이름은 ["+str7+"]입니다.");
-		//String.trim() : String 객체에 저장된 문자열에서 앞과 뒤에 존재하는 모든 공백을 제거하여 반환하는 메소드
+		//★String.trim() : String 객체에 저장된 문자열에서 앞과 뒤에 존재하는 모든 공백을 제거하여 반환하는 메소드
 		System.out.println("입력된 이름은 ["+str7.trim()+"]입니다.");
 		System.out.println("==============================================================");
 		
@@ -141,7 +141,7 @@ public class StringApp {
 		String str8="   임    꺽     정      ";
 
 		System.out.println("입력된 이름은 ["+str8+"]입니다.");
-		//String.replace(String regEx, String replacement) : String 객체에 저장된 문자열에서
+		//★String.replace(String regEx, String replacement) : String 객체에 저장된 문자열에서
 		//검색 문자열(정규표현식)을 찾아 치환 문자열로 변경하여 반환하는 메소드
 		System.out.println("입력된 이름은 ["+str8.replace(" ", "").replace("꺽", "걱")+"]입니다.");
 		System.out.println("==============================================================");
@@ -154,7 +154,7 @@ public class StringApp {
 		System.out.println("==============================================================");
 		
 		
-		///String.split(String regEx) : String 객체에 저장된 문자열을 매개변수로 전달받은
+		///★String.split(String regEx) : String 객체에 저장된 문자열을 매개변수로 전달받은
 		//문자열(정규표현식)로 구분 분리하여 문자열 배열로 반환하는 메소드
 		// => 정규표현식에서 사용되는 메타문자를 일반문자로 변환하여 사용하기 위해 [\\]를 사용하여 회피문자로 처리하여 표현
 		String[] numArray=str9.split("-");
@@ -167,7 +167,7 @@ public class StringApp {
 		
 		
 		
-		//String.substring(int beginIndex, int endIndex) : String 객체에 저장된 문자열에서
+		//★String.substring(int beginIndex, int endIndex) : String 객체에 저장된 문자열에서
 		//시작첨자(문자포함)에서 종료첨자(문자미포함)까지의 문자열을 분리하여 반환하는 메소드
 		System.out.println("전화번호 앞부분 = "+str9.substring(0, 3));
 		System.out.println("전화번호 중간부분 = "+str9.substring(4, 8));
@@ -178,7 +178,7 @@ public class StringApp {
 		
 		
 		
-		//String.valueOf(Object obj) : 매개변수로 전달된 모든 자료형의 값을 문자열(String 
+		//★String.valueOf(Object obj) : 매개변수로 전달된 모든 자료형의 값을 문자열(String 
 		//객체)로 변환하여 반환하는 메소드
 		//String numString=String.valueOf(100);
 		//""+값 또는 값+"" 형식으로 모든 자료형의 값을 문자열과 결합하여 문자열로 사용 가능
