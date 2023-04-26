b3df83a28d7b61d07c92ee3b639f35ad1e1771d8
itwill <itwill@DESKTOP-7688MVJ>
2023-04-26 11:19:49 +0900

    20Day

--- java_3/src/xyz/itwill/io/ObjectInputStreamApp.java
+++ java_3/src/xyz/itwill/io/ObjectInputStreamApp.java
@@ -8,12 +8,10 @@ import java.util.List;
 
 public class ObjectInputStreamApp {
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		//ObjectInputStream 클래스 : InputStream 객체를 전달받아 원시데이타를 Java 객체로  
-		//제공받기 위한 입력스트림을 생성하기 위한 클래스
+		//ObjectInputStream 클래스 : InputStream 객체를 전달받아 원시데이타를 Java 객체로 제공받기 위한 입력스트림을 생성하기 위한 클래스
 		ObjectInputStream in=new ObjectInputStream(new FileInputStream("c:/data/object.txt"));
 		
-		//ObjectInputStream.readObject() : 입력스트림으로 원시데이타를 제공받아 Java 객체로
-		//변환하여 반환하는 메소드
+		//ObjectInputStream.readObject() : 입력스트림으로 원시데이타를 제공받아 Java 객체로 변환하여 반환하는 메소드
 		// => Object 타입의 객체로 반환되므로 반드시 명시적 객체 형변환 사용
 		// => 반환되는 객체에 대한 클래스가 없는 경우 ClassNotFoundException 발생
 		String string=(String)in.readObject();
