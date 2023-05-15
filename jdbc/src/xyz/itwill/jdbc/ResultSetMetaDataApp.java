aa4b5b0f8863d2cff66a6f74ba6aa5913f294cc7
user <user@BOOK-LCCUUB9T5H>
2023-05-15 12:54:55 +0900

    33Day

--- jdbc/src/xyz/itwill/jdbc/ResultSetMetaDataApp.java
+++ jdbc/src/xyz/itwill/jdbc/ResultSetMetaDataApp.java
@@ -2,6 +2,7 @@ package xyz.itwill.jdbc;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -16,6 +17,15 @@ public class ResultSetMetaDataApp {
 		while(rs.next()) {
 			System.out.println("학번 = "+rs.getInt("no")+", 이름 = "+rs.getString("name"));
 		}
+		System.out.println("=====================================================================");
+		//ResultSet.getMetaData() : 검색행(ResultSet 객체)에 대한 부가적인 정보가 저장된 ResultSetMetaData 객체를 반환하는 메소드
+		ResultSetMetaData rsmd=rs.getMetaData();
+		
+		//
+		
+		
+		
+		
 		System.out.println("=====================================================================");
 		ConnectionFactory.close(con, stmt, rs);
 	}
