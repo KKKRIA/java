f86dc6fbc8aad86d192acde68f00c91093f68558
user <dnjsgml5153@naver.com>
2023-05-18 19:30:35 +0900

    35Day

--- jdbc/src/xyz/itwill/student/WindowBuilderApp.java
+++ jdbc/src/xyz/itwill/student/WindowBuilderApp.java
@@ -1,21 +1,33 @@
 package xyz.itwill.student;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
+import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
 import javax.swing.border.EmptyBorder;
-import java.awt.BorderLayout;
-import javax.swing.JButton;
-import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JTextArea;
 
 public class WindowBuilderApp extends JFrame {
+	private static final long serialVersionUID = 1L;
 
 	private JPanel contentPane;
+	private JButton red;
+	private JButton green;
+	private JButton blue;
+	private JTextPane textPane;
+	private JTextField textField;
 
+	Test dialog = new Test(this);
 	/**
 	 * Launch the application.
 	 */
@@ -37,23 +49,85 @@ public class WindowBuilderApp extends JFrame {
 	 */
 	public WindowBuilderApp() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 756, 479);
+		setBounds(100, 100, 400, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(33, 29, 255, 157);
-		contentPane.add(panel);
+		panel.setBackground(new Color(192, 192, 192));
+		contentPane.add(panel, BorderLayout.NORTH);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(12, 223, 216, 68);
-		contentPane.add(btnNewButton);
+		red = new JButton("빨간색"); 
+		red.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dialog.setVisible(true);
+			}
+		});
+		red.setForeground(new Color(255, 0, 0));
+		red.setFont(new Font("굴림체", Font.PLAIN, 20));
+		panel.add(red);
 		
-		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(191, 312, 97, 23);
-		contentPane.add(btnNewButton_1);
+		green = new JButton("초록색");
+		green.setForeground(new Color(0, 255, 0));
+		green.setFont(new Font("굴림체", Font.PLAIN, 20));
+		panel.add(green);
+		
+		blue = new JButton("파란색");
+		blue.setForeground(new Color(0, 0, 255));
+		blue.setFont(new Font("굴림체", Font.PLAIN, 20));
+		panel.add(blue);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		
+		textPane = new JTextPane();
+		scrollPane.setViewportView(textPane);
+		
+		textField = new JTextField();
+		contentPane.add(textField, BorderLayout.SOUTH);
+		textField.setColumns(10);
+		
+	}
+	
+	public class Test extends JDialog {
+		private static final long serialVersionUID = 1L;
+		private final JPanel contentPanel = new JPanel();
+	
+		public Test(JFrame frame) {
+			super(frame, true);
+			
+			setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
+			
+			setBounds(100, 100, 450, 300);
+			getContentPane().setLayout(new BorderLayout());
+			contentPanel.setLayout(new FlowLayout());
+			contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+			getContentPane().add(contentPanel, BorderLayout.CENTER);
+			{
+				JPanel buttonPane = new JPanel();
+				buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+				getContentPane().add(buttonPane, BorderLayout.SOUTH);
+				{
+					JButton okButton = new JButton("OK");
+					okButton.setActionCommand("OK");
+					buttonPane.add(okButton);
+					getRootPane().setDefaultButton(okButton);
+				}
+				{
+					JButton cancelButton = new JButton("Cancel");
+					cancelButton.setActionCommand("Cancel");
+					buttonPane.add(cancelButton);
+					cancelButton.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent e) {
+							dialog.setVisible(false);
+						}
+					});
+				}
+			}
+		}
+
 	}
 }
