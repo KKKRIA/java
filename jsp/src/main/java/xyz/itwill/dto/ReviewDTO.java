5ca3899a23bd8145f9d965af706a234dfa41ee44
user <dnjsgml5153@naver.com>
2023-06-29 18:15:28 +0900

    60Day

--- jsp/src/main/java/xyz/itwill/dto/ReviewDTO.java
+++ jsp/src/main/java/xyz/itwill/dto/ReviewDTO.java
@@ -3,6 +3,7 @@ package xyz.itwill.dto;
 //create table review(num number primary key, id varchar2(30), subject varchar2(500)
 //    , content varchar2(4000), regdate date, readcount number, ref number(4)
 //	  , restep number(4), relevel number(4), ip varchar2(20), status number(1));
+
 //create sequence review_seq; 
 
 /* 
