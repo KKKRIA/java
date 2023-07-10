17ee3976f498c4aa48b2c201df0bd1fd0b7af387
KKKRIA <dnjsgml5153@naver.com>
2023-07-10 17:52:42 +0900

    67Day

---
+++ mvc/src/main/java/xyz/itwill/mvc/WriteModel.java
@@ -0,0 +1,16 @@
+package xyz.itwill.mvc;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WriteModel implements Action{
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
