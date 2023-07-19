b3a61a9b2eeedcc126c4d19181bdddbe82c2daf5
ria08 <dnjsgml5153@naver.com>
2023-07-19 13:25:51 +0900

    74Day

--- mybatis/src/main/java/xyz/itwill/mapper/MyCommentMapper.java
+++ mybatis/src/main/java/xyz/itwill/mapper/MyCommentMapper.java
@@ -5,6 +5,8 @@ import java.util.List;
 import xyz.itwill.dto.MyComment1;
 import xyz.itwill.dto.MyComment2;
 import xyz.itwill.dto.MyComment3;
+import xyz.itwill.dto.MyCommentUser1;
+import xyz.itwill.dto.MyCommentUser2;
 
 public interface MyCommentMapper {
 	int insertComment1(MyComment1 comment);
@@ -12,4 +14,6 @@ public interface MyCommentMapper {
 	List<MyComment1> selectCommentList1();
 	List<MyComment2> selectCommentList2();
 	List<MyComment3> selectCommentList3();
+	List<MyCommentUser1> selectCommentUserList1();
+	List<MyCommentUser2> selectCommentUserList2();
 }
