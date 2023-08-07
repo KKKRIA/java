45ab3430815e6aad1842d15c07173f71d565666a
KKKRIA <dnjsgml5153@naver.com>
2023-08-07 13:12:02 +0900

    87Day

---
+++ spring/src/main/java/xyz/itwill10/controller/RedirectController.java
@@ -0,0 +1,16 @@
+package xyz.itwill10.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RedirectController {
+	@RequestMapping("/forward_move")
+	private String forward(Model model) {
+		
+		model.addAttribute("name", "홍길동");
+		
+		return "display_forward";
+	}
+}
