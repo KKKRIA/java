743b4d55cef59d130057b7203a715a00260cc8f1
KKKRIA <dnjsgml5153@naver.com>
2023-08-07 19:09:48 +0900

    87Day

--- spring/src/main/java/xyz/itwill10/controller/RedirectController.java
+++ spring/src/main/java/xyz/itwill10/controller/RedirectController.java
@@ -7,10 +7,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class RedirectController {
 	@RequestMapping("/forward_move")
-	private String forward(Model model) {
-		
+	public String forward(Model model) {
+		//Model 객체를 사용하여 뷰(View)에게 제공할 객체를 속성값으로 저장 - Request Scope
 		model.addAttribute("name", "홍길동");
 		
-		return "display_forward"; 
+		//뷰를 생성하기 위한 뷰이름(ViewName) 반환
+		// => Front Controller(DispatchServlet 클래스)는 제공받은 뷰이름을 InternalResourceViewResolver 
+		//객체를 사용하여 JSP 문서로 변환하고 JSP 문서로 포워드 이동하여 응답 처리
+		//포워드 이동 : 서버 내부에서 현재 웹프로그램의 스레드를 다른 웹프로그램으로 이동하여 응답 처리
+		// => 클라이언트의 요청 URL 주소는 변경되지 않으며 Request Scope 속성값을 객체로 제공받아 사용 가능
+		return "display_forward";
 	}
+	
+	/*
+	@RequestMapping("/redirect_move")
+	public String redirect(Model model) {
+		model.addAttribute("name", "임꺽정");
+		return "display_redirect";
+	}
+	
+	@RequestMapping("/redirect")
+	public String redirect() {
+		//요청 처리 메소드의 반환값(뷰이름)에 
+		return "redirect:/redirect_move";
+	}
+	*/
+	
+	
+	
 }
+
+
+
+
+
+
+
