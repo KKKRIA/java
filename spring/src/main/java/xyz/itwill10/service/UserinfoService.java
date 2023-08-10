61383d352eaf686392adf5584fde11e979d58fb1
KKKRIA <dnjsgml5153@naver.com>
2023-08-10 14:59:10 +0900

    90Day

--- spring/src/main/java/xyz/itwill10/service/UserinfoService.java
+++ spring/src/main/java/xyz/itwill10/service/UserinfoService.java
@@ -3,12 +3,12 @@ package xyz.itwill10.service;
 import java.util.List;
 
 import xyz.itwill10.dto.Userinfo;
-import xyz.itwill10.exception.ExistuUserinfoException;
+import xyz.itwill10.exception.ExistsUserinfoException;
 import xyz.itwill10.exception.LoginAuthFailException;
 import xyz.itwill10.exception.UserinfoNotFoundException;
 
-public interface UserinfoSercice {
-	void addUserinfo(Userinfo userinfo ) throws ExistuUserinfoException;
+public interface UserinfoService {
+	void addUserinfo(Userinfo userinfo ) throws ExistsUserinfoException;
 	void modifyUserinfo(Userinfo userinfo) throws UserinfoNotFoundException;
 	void removeUserinfo(String userinfo) throws UserinfoNotFoundException;
 	Userinfo getUserinfo(String userinfo) throws UserinfoNotFoundException;
