e281a614eccbfd49f0c77c8c372820f7587c851b
KKKRIA <dnjsgml5153@naver.com>
2023-08-10 15:54:28 +0900

    90Day

--- spring/src/main/java/xyz/itwill10/exception/LoginAuthFailException.java
+++ spring/src/main/java/xyz/itwill10/exception/LoginAuthFailException.java
@@ -2,12 +2,15 @@ package xyz.itwill10.exception;
 
 import lombok.Getter;
 
-//로그인에 대한 인증이 실패한 경우
-public class LoginAuthFailException extends Exception{
+//로그인 처리할때 사용자로부터 입력받은 아이디와 비밀번호에 대한 인증이 실패한 경우 발생될 
+//예외를 처리하기 위한 예외 클래스
+public class LoginAuthFailException extends Exception {
 	private static final long serialVersionUID = 1L;
-	
+
+	//예외처리에 필요한 값을 저장하기 위한 필드
+	// => 사용자부터 입력받은 아이디를 저장하기 위한 필드
 	@Getter
-	private String Userid;
+	private String userid;
 	
 	public LoginAuthFailException() {
 		// TODO Auto-generated constructor stub
@@ -15,7 +18,6 @@ public class LoginAuthFailException extends Exception{
 	
 	public LoginAuthFailException(String message, String userid) {
 		super(message);
-		this.Userid=userid;
+		this.userid=userid;
 	}
-	
 }
