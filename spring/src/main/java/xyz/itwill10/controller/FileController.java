736197f86383c333fc1b697ddd48b6f95e721f9f
KKKRIA <dnjsgml5153@naver.com>
2023-08-11 12:57:34 +0900

    91Day

---
+++ spring/src/main/java/xyz/itwill10/controller/FileController.java
@@ -0,0 +1,10 @@
+package xyz.itwill10.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/file")
+public class FileController {
+
+}
